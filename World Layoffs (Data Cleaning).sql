dc6b99ee76142f3a25dc440ac1a06696d9fd3287
MarkCraig22 <markcraig107@gmail.com>
2024-05-29 11:05:42 +0200

    Create World Layoffs (Data Cleaning).sql

---
+++ World Layoffs (Data Cleaning).sql
@@ -0,0 +1,191 @@
+											  --- Data Cleaning --- 
+
+SELECT *
+FROM layoffs;
+
+-- Steps:
+-- 1. Remove duplicates
+-- 2. Standardize data
+-- 3. Blank and NUll values
+-- 4. Remove columns
+
+----------------------------------------------------------------------------------------------------------------------------
+
+-- Create staging table 
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+SELECT *
+FROM layoffs_staging;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+										--- 1. Removing Duplicates ---
+
+SELECT *,
+	ROW_NUMBER() OVER (
+		PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`  -- Partitioning criteria
+		ORDER BY (SELECT NULL)  -- Dummy ORDER BY clause - don't need specific ordering
+	) AS row_num
+FROM layoffs_staging;
+
+WITH duplicate_cte AS
+(
+SELECT *,
+	ROW_NUMBER() OVER (
+		PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions  -- Partitioning criteria
+		ORDER BY (SELECT NULL)  -- Dummy ORDER BY clause - don't need specific ordering
+	) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte
+WHERE row_num > 1; 
+
+
+ALTER TABLE layoffs_staging
+DROP COLUMN MyUnknownColumn; -- Deleted an extra column that I created in CSV somehow
+
+
+CREATE TABLE `layoffs_staging3` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci; -- Copied create statement from clipboard
+
+SELECT * 
+FROM layoffs_staging3;
+
+INSERT INTO layoffs_staging3
+SELECT *,
+	ROW_NUMBER() OVER (
+		PARTITION BY company, location, industry, total_laid_off, percentage_laid_off, `date`, stage, country, funds_raised_millions  -- Partitioning criteria
+		ORDER BY (SELECT NULL)  -- Dummy ORDER BY clause - don't need specific ordering
+	) AS row_num
+FROM layoffs_staging;
+
+SET sql_safe_updates = 0; -- Allow myself to delete 
+
+DELETE
+FROM layoffs_staging3
+WHERE row_num > 1; -- Delete the duplicates
+
+SELECT *
+FROM layoffs_staging3
+WHERE row_num > 1;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+-										--- 2. Standardizing Data ---
+SELECT company, TRIM(company)
+FROM layoffs_staging3;
+
+UPDATE layoffs_staging3
+SET company = TRIM(company);
+
+SELECT *
+FROM layoffs_staging3
+WHERE industry LIKE 'Crypto%';
+
+UPDATE layoffs_staging3
+SET industry = 'Crypto'
+WHERE industry LIKE 'Crypto%';
+
+SELECT DISTINCT(country)
+FROM layoffs_staging3
+ORDER BY 1;
+
+SELECT DISTINCT country
+FROM layoffs_staging3
+WHERE country LIKE 'United State%';
+
+UPDATE layoffs_staging3
+SET country = 'United States'
+WHERE country LIKE 'United State%';
+
+SELECT `date`,
+STR_TO_DATE(`date`, '%m/%d/%Y')
+FROM layoffs_staging3;
+
+UPDATE layoffs_staging3
+SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
+
+SELECT `date`
+FROM layoffs_staging3;
+
+ALTER TABLE layoffs_staging3
+MODIFY COLUMN `date` DATE;
+
+SELECT *
+FROM layoffs_staging3;
+
+-----------------------------------------------------------------------------------------------------------------------------
+
+										--- 3. NUll and Blank Values ---
+SELECT *
+FROM layoffs_staging3
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+UPDATE layoffs_staging3
+SET industry = NULL 
+WHERE industry = '';
+
+SELECT *
+FROM layoffs_staging3
+WHERE industry IS NULL 
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging3
+WHERE company LIKE 'Bally%';
+
+
+SELECT *
+FROM layoffs_staging3 t1
+JOIN layoffs_staging3 t2 
+	ON t1.company = t2.company
+	AND t1.location = t2.location
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+UPDATE layoffs_staging3 t1
+JOIN layoffs_staging3 t2 
+	ON t1.company = t2.company
+SET t1.industry = t2.industry 
+WHERE t1.industry IS NULL 
+AND t2.industry IS NOT NULL;
+
+----------------------------------------------------------------------------------------------------------------------------
+
+									--- 4. Remove Columns or Rows ---
+
+SELECT *
+FROM layoffs_staging3
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+DELETE
+FROM layoffs_staging3
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL;
+
+SELECT * 
+FROM layoffs_staging3;
+
+ALTER TABLE layoffs_staging3
+DROP COLUMN row_num;
+
+-----------------------------------------------------------------------------------------------------------------------------
